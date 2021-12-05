8653efc7e6f9e9ace658e1c8acc301cd6fe5db07
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2021-12-05 21:25:02 +0200

    Slides

---
+++ app.js
@@ -0,0 +1,9 @@
+const slides = document.querySelectorAll('.slide')
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses()
+
+        slide.classList.add('active')
+    })
+}
